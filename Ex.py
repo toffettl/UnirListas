020ba35ce164922c1be097976f708b62742eb230
Felipe Toffetti <139416594+toffettl@users.noreply.github.com>
2025-02-04 00:16:25 -0300

    Exercício feito

---
+++ Ex.py
@@ -0,0 +1,5 @@
+from itertools import zip_longest
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
+print(list(zip(l1, l2)))
+print(list(zip_longest(l1, l2, fillvalue='SEM CIDADE')))
